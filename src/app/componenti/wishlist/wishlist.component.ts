5b027f4936f03708c12a3cca61bcf9d968dc6931
Yohannes Luminari <67595372+yohannesluminari@users.noreply.github.com>
2025-02-13 17:47:50 +0100

    wishlist

--- src/app/componenti/wishlist/wishlist.component.ts
+++ src/app/componenti/wishlist/wishlist.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { WishlistService } from '../../servizi/wishlist/wishlist.service';
 
+interface Prodotto {
+  immagineProdotto: string;
+  titolo: string;
+  descrizione: string;
+  prezzo: number;
+  formato: string;
+  annoPubblicazione: number;
+}
+
 @Component({
   selector: 'app-wishlist',
   standalone: false,
@@ -19,5 +28,23 @@ export class WishlistComponent implements OnInit {
       // Filtra le wishlist per mostrare solo quella dell'utente corrente
       this.wishlist = data.filter(item => item.cliente.idCliente === this.currentUserId);
     });
+  }
+
+
+  addToWishlist(prodotto: Prodotto): void {
+    console.log('Aggiunto alla wishlist', prodotto);
+  }
+
+  removeFromWishlist(prodotto: Prodotto): void {
+
+    console.log('Rimosso dalla wishlist', prodotto);
+    this.wishlist.forEach(item => {
+
+      item.prodotti = item.prodotti.filter((p: Prodotto) => p !== prodotto);
+    });
+  }
+
+  clearAllFromWishlist(item: any): void {
+    console.log('Tutti i prodotti rimossi dalla wishlist');
   }
 }
