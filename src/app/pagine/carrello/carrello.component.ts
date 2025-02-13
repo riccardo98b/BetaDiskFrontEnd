b1c830e97df2e4da46a482405b5858827f8a158c
Michel <michel.zanona@gmail.com>
2025-02-13 17:55:45 +0100

    svuota carrello

--- src/app/pagine/carrello/carrello.component.ts
+++ src/app/pagine/carrello/carrello.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CarrelloService } from '../../servizi/carrello/carrello.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrello',
@@ -13,16 +14,39 @@ export class CarrelloComponent implements OnInit {
   prodotti: any;
   totale: number = 0;
   idCliente = 1;
+  msg: string = "";
+  rc:boolean = true;
 
-  constructor(private serv: CarrelloService) {}
+  constructor(private serv: CarrelloService,
+    private router : Router
+  ) {}
 
   ngOnInit(): void {
    this.serv.listaProdotti(this.idCliente).subscribe((r : any) => {
-    this.totale = r.dati.totale;
-    this.prodotti = r.dati.prodotti;
-    console.log(this.prodotti);
+
+    if (r.rc) {
+      this.totale = r.dati.totale;
+      this.prodotti = r.dati.prodotti;
+    } else {
+      this.rc = r.rc;
+      this.msg = r.msg;
+    }
    })
   }
 
+  svuotaCarrello() {
+    let request = {
+      "idCliente" : this.idCliente
+    };
+    this.serv.svuotaCarrello(request).subscribe((r:any) => {
+      this.msg = r.msg;
+      this.rc = r.rc;
+      this.router.navigate(["/carrello"]).then(() => {
+        setTimeout(() => {
+          window.location.reload();
+        }, 2000); 
+      });
+    })
+  }
   
 }
