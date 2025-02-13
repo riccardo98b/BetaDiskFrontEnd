a7070cb9557db760c7cb64a87254e70b8eda5074
Michel <michel.zanona@gmail.com>
2025-02-13 16:34:27 +0100

    dichirazione servizi carrello

--- src/app/servizi/carrello/carrello.service.ts
+++ src/app/servizi/carrello/carrello.service.ts
@@ -13,4 +13,17 @@ export class CarrelloService {
   listaProdotti(id: number) {
     return this.http.get(this.url + "lista?id=" + id)
   }
+
+  addProdotto(body: {}){
+    return this.http.post(this.url + "add", body)
+  }
+
+  removeProdotto(body: {}){
+    return this.http.post(this.url + "remove", body)
+  }
+
+  svuotaCarrello(body: {}) {
+    return this.http.post(this.url + "delete", body)
+  }
+
 }
