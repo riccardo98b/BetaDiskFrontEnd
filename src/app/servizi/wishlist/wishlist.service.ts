b40e97ef7d1d0ae1f8cbe8b3680ac11941b8a007
Yohannes Luminari <67595372+yohannesluminari@users.noreply.github.com>
2025-02-14 17:54:24 +0100

    WISHLIST collegamento con backend listAll

--- src/app/servizi/wishlist/wishlist.service.ts
+++ src/app/servizi/wishlist/wishlist.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Prodotto } from '../../interfacce/Prodotto';
+
+interface ApiResponse {
+  rc: boolean;
+  msg: string;
+  dati: Prodotto[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class WishlistService {
-  private apiUrl = 'assets/wishlist.json';
+  private apiUrl = 'http://localhost:9090/rest/wishlist/getAllProducts';
 
   constructor(private http: HttpClient) {}
 
-  getWishlist(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getWishlist(idCliente: number): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(`${this.apiUrl}?idCliente=${idCliente}`);
   }
 }
