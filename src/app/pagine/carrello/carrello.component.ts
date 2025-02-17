e698205075d4f1697198ac7c41fc8633940c3e42
Michel <michel.zanona@gmail.com>
2025-02-17 11:33:06 +0100

    funzioni card carrello complete

--- src/app/pagine/carrello/carrello.component.ts
+++ src/app/pagine/carrello/carrello.component.ts
@@ -11,21 +11,29 @@ import { Router } from '@angular/router';
 export class CarrelloComponent implements OnInit {
   prodotti: any;
   totale: number = 0;
-  idCliente = 2;
+  idCliente = 1;
   msg: string = '';
   rc: boolean = true;
+  isLoading: boolean;
 
   constructor(private serv: CarrelloService, private router: Router) {}
 
   ngOnInit(): void {
+    this.isLoading = true;
     this.serv.listaProdotti(this.idCliente).subscribe((r: any) => {
       if (r.rc) {
         this.totale = r.dati.totale;
-        this.prodotti = r.dati.prodotti;
+        if (this.totale == 0) {
+          this.rc = false;
+          this.msg= "Il carrello è vuoto, inizia a fare acquisti da noi!"
+        } else {
+          this.prodotti = r.dati.prodotti;
+        }
       } else {
         this.rc = r.rc;
         this.msg = r.msg;
       }
+      this.isLoading = false
     });
   }
 
@@ -39,8 +47,63 @@ export class CarrelloComponent implements OnInit {
       this.router.navigate(['/carrello']).then(() => {
         setTimeout(() => {
           window.location.reload();
-        }, 2000);
+        }, 1500);
       });
     });
+  }
+
+  dettagliProdotto(idProdotto: number) {
+    this.router.navigate(['/dettaglio-prodotto', idProdotto]);
+  }
+
+  rimuoviProdotto(idProdotto: number) {
+    let request = {
+      idCliente : this.idCliente,
+      idProdotto : idProdotto,
+      quantita: 1
+    }
+    this.serv.removeProdotto(request).subscribe((r:any) => {
+      this.msg = r.msg;
+      this.rc = r.rc;
+      this.router.navigate(['/carrello']).then(() => {
+        setTimeout(() => {
+          window.location.reload();
+        }, 1500);
+      });
+    })
+  }
+
+  aggiungiProdotto(idProdotto: number) {
+    let request = {
+      idCliente : this.idCliente,
+      idProdotto : idProdotto,
+      quantita: 1
+    }
+    this.serv.addProdotto(request).subscribe((r:any) => {
+      this.msg = r.msg;
+      this.rc = r.rc;
+      this.router.navigate(['/carrello']).then(() => {
+        setTimeout(() => {
+          window.location.reload();
+        }, 1500);
+      });
+    })
+  }
+
+  eliminaProdotto(idProdotto: number, quantita: number){
+    let request = {
+      idCliente : this.idCliente,
+      idProdotto : idProdotto,
+      quantita: quantita
+    }
+    this.serv.removeProdotto(request).subscribe((r:any) => {
+      this.msg = r.msg;
+      this.rc = r.rc;
+      this.router.navigate(['/carrello']).then(() => {
+        setTimeout(() => {
+          window.location.reload();
+        }, 1500);
+      });
+    })
   }
 }
