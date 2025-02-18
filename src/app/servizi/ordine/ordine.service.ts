1329139d4f524eb248a92cbaa23c15835c2bab91
Michel <michel.zanona@gmail.com>
2025-02-18 10:59:31 +0100

    invio ordine

--- src/app/servizi/ordine/ordine.service.ts
+++ src/app/servizi/ordine/ordine.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class OrdineService {
 
-  constructor() { }
+  url = "http://localhost:9090/rest/ordine/"
+  
+  constructor(private http: HttpClient ) { }
+
+  inviaOrdine(body: {}){
+    return this.http.post(this.url + "create", body)
+  }
+
 }
